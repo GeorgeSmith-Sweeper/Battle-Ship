73749e899ebf364d2c1376f2cc3cf8118c5792fb
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-10-05 22:45:35 -0400

    correctly tested user_prompt

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,4 +1,5 @@
 from unittest import TestCase, mock
+from unittest.mock import patch
 from core import ui
 from io import StringIO
 
@@ -8,6 +9,17 @@ class TestPrintWrapper(TestCase):
         with mock.patch('sys.stdout', new=StringIO()) as fake_stdout:
             message.terminal_msg('hello')
         self.assertEqual(fake_stdout.getvalue(), 'hello\n')
+
+class TestInputWrapper(TestCase):
+    def answer(self):
+        prompt = ui.Messages()
+        testAnswer = prompt.prompt_user("enter hello")
+        if testAnswer == 'hello':
+            return 'it works'
+        
+    @patch('core.ui.Messages.prompt_user', return_value="hello")
+    def test_can_get_input(self, imput):
+        self.assertEqual(self.answer(), 'it works')
 
 class TestDisplayBoard(TestCase):
     def test_board_is_displayed_as_a_grid(self):
