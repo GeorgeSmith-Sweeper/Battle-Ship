811cde4a6956742ec17871f9e5d0d861d0b7ed26
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-10-05 21:41:43 -0400

    add start method

--- play.py
+++ play.py
@@ -1,1 +1,15 @@
-   
+from core import board
+from core import ui
+
+
+class Game(ui.BoardPresenter, ui.Messages):
+    def __init__(self, board):
+        self.board = board
+
+    def start(self):
+        self.terminal_msg(self.display_terminal_board(self.board.state)) 
+
+if __name__ == "__main__":
+    new_board = board.Board()
+    new_game = Game(new_board)
+    new_game.start()
--- tests/test_play.py
+++ tests/test_play.py
@@ -1,1 +1,14 @@
 from unittest import TestCase
+from unittest.mock import patch, MagicMock
+from play import Game
+from core import ui
+from core import board
+
+class TestGameLoop:
+    def test_game_starts(self):
+        with patch.object(Game, 'display_terminal_board', return_value=None) as mock_method:
+            new_board = board.Board()
+            new_game = Game(new_board)
+            new_game.start()
+            mock_method.assert_called()
+             
