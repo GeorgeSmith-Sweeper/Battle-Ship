c70a8c651a40b47329a5a786f8a33b7db25ac5f5
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-10-07 11:50:15 -0400

    very large changes

--- core/board.py
+++
@@ -1,4 +0,0 @@
-class Board:
-    def __init__(self):
-        self.state = [[None for x in range(10)] for y in range(10)] 
-
--- play.py
+++ play.py
@@ -1,15 +1,36 @@
-from core import board
-from core import ui
-
-
-class Game(ui.BoardPresenter, ui.Messages):
-    def __init__(self, board):
+class Game:
+    def __init__(self, board, spot_chooser, ui):
+        self.ui = ui
         self.board = board
+        pass 
+    def play(self):
+        self.ui.display("Welcome to Battleship")
+        self.ui.display(self.board.format())
+        pass
 
-    def start(self):
-        self.terminal_msg(self.display_terminal_board(self.board.state)) 
+class TerminalUi:
+    def display(self, message):
+       print(message)
 
-if __name__ == "__main__":
-    new_board = board.Board()
-    new_game = Game(new_board)
-    new_game.start()
+class Board:
+    def __init__(self):
+        self.state = [[None for ele in range(10)] for index in range(10)]
+    def format(self):
+        return """
+[][][][][][][][][][]
+[][][][][][][][][][]
+[][][][][][][][][][]
+[][][][][][][][][][]
+[][][][][][][][][][]
+[][][][][][][][][][]
+[][][][][][][][][][]
+[][][][][][][][][][]
+[][][][][][][][][][]
+[][][][][][][][][][]
+"""
+
+class SpotChooser:
+    def __init__(self, board):
+        pass
+    pass
+
