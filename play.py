511b391c31ae54089f22c222de1c8bb9eb9571c1
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-10-08 18:10:19 -0400

    board can update

--- play.py
+++ play.py
@@ -49,4 +49,30 @@ class Board:
         raise ValueError
 
     def update(self, user_shot_choice):
-        pass
+        letters = {
+                'A': 0, 
+                'B': 1, 
+                'C': 2, 
+                'D': 3, 
+                'E': 4, 
+                'F': 5, 
+                'G': 6, 
+                'H': 7, 
+                'I': 8, 
+                'J': 9
+                }
+        numbers = {
+                '1': 0, 
+                '2': 1, 
+                '3': 2, 
+                '4': 3, 
+                '5': 4, 
+                '6': 5, 
+                '7': 6, 
+                '8': 7, 
+                '9': 8, 
+                '10': 9
+                }
+        user_letter, user_num = user_shot_choice
+        self.state[letters[user_letter]][numbers[user_num]] = 'x'
+
