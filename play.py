723c5db0e814585e32e80cc42dcc95ee4c1253e1
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-10-12 13:46:05 -0400

    ships have a size and symbol

--- play.py
+++ play.py
@@ -1,14 +1,16 @@
-from core import board, ui, validate
+from core import board, ui, validate, ships
 
 class Game:
-    def __init__(self, board, ui, validate):
+    def __init__(self, board, ui, validate, ships):
         self.ui = ui
         self.board = board
         self.validate = validate
+        self.ships = ships
 
     def play(self):
         self.ui.display("Welcome to Battleship")
         board_full = False
+        self.ships.place_on_board(self.board.state)
         while not board_full:
             board_full = self.validate.board_full(self.board.state)
             self.ui.display("Take your best shot")
@@ -21,6 +23,7 @@ if __name__ == "__main__":
     board = board.Board()
     ui = ui.TerminalUi()
     validate = validate.Validate()
-    game = Game(board, ui, validate)
+    ships = ships.Ships()
+    game = Game(board, ui, validate, ships)
     game.play()
 
--- tests/test_ships.py
+++ tests/test_ships.py
@@ -1,12 +1,35 @@
-'''
-class ComputerShips(TestCase):
-    def test_create_ships_places_the_correct_ships_on_the_board(self):
-        board = Board()
+from unittest import TestCase, mock
+from unittest.mock import patch, MagicMock
+from core.board import Board
+from core.ships import Ships
+
+class TestShips(TestCase):
+    def test_ships_is_initialized_with_a_five_unique_ships(self):
         ships = Ships()
-        ship_added = Ship_Placer(board, ships)
+        aircraft_carrier_symbol = 'AC'
+        battleship_symbol = 'B'
+        cruiser_symbol = 'C'
+        submarine_symbol = 'S'
+        destroyer_symbol = 'D'
 
-        board_state = [[]]
-        board_state_after_ship_was_added = [['AC', 'AC', 'AC', 'AC', 'AC']]
+        self.assertEqual(ships.aircraft_carrier['symbol'], aircraft_carrier_symbol)
+        self.assertEqual(ships.destroyer['symbol'], destroyer_symbol)
+        self.assertEqual(ships.cruiser['symbol'], cruiser_symbol)
+        self.assertEqual(ships.submarine['symbol'], submarine_symbol)
+        self.assertEqual(ships.battleship['symbol'], battleship_symbol)
+        
+    def test_each_ship_has_a_size(self):
+        ships = Ships()
+        aircraft_carrier_size = 5
+        battleship_size = 4
+        cruiser_size = 3
+        submarine_size = 3
+        destroyer_size = 2
+    
+        self.assertEqual(ships.aircraft_carrier['size'], aircraft_carrier_size)
+        self.assertEqual(ships.battleship['size'], battleship_size)
+        self.assertEqual(ships.cruiser['size'], cruiser_size)
+        self.assertEqual(ships.submarine['size'], submarine_size)
+        self.assertEqual(ships.destroyer['size'], destroyer_size)
 
-        ship.aircraft_carrier = MagicMock()
-'''
+    
