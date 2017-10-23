8218ff253e2bef87424959eadaaf6f60bacad879
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-10-23 13:06:49 -0400

    ships have health

--- core/ships.py
+++ core/ships.py
@@ -6,26 +6,31 @@ class Ships:
                 'name': 'Aircraft Carrier',
                 'symbol': 'AC',
                 'size': 5,
+                'health': 5,
                 }
         self.battleship = {
                 'name': 'Battleship',
                 'symbol': 'B',
                 'size': 4,
+                'health': 4,
                 }
         self.cruiser = {
                 'name': 'Cruiser',
                 'symbol': 'C',
-                'size': 3
+                'size': 3,
+                'health': 3,
                 }
         self.submarine = {
                 'name': 'Submarine',
                 'symbol': 'S',
                 'size': 3,
+                'health': 3,
                 }
         self.destroyer = {
                 'name': 'Destroyer',
                 'symbol': 'D',
                 'size': 2,
+                'health': 2,
                 }
         self.all_ships = [
                 self.aircraft_carrier, 
