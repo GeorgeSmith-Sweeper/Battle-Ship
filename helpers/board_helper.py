e91715b1e0c1f5ed32b4c34cc8e9d0340c906a60
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-10-27 10:53:57 -0400

    create boardhelper function

--- helpers/board_helper.py
+++ helpers/board_helper.py
@@ -1,17 +1,9 @@
-from unittest import TestCase, mock
-from unittest.mock import patch, MagicMock
-from core.board import Board
-from core.placement import Place
-from core.validate import Validate
-from core.ships import Ships
-from helpers.board_helper import BoardHelper
+class BoardHelper:
+    def __init__(self, ships):
+        self.ships = ships
 
-class TestBoard(TestCase):
-
-    def setUp(self):
-       self.board = Board()
-       self.ships = Ships()
-       self.empty_board = [
+    def generate_empty_board(self):
+       empty_board = [
                [None, None, None, None, None, None, None, None, None, None],
                [None, None, None, None, None, None, None, None, None, None],
                [None, None, None, None, None, None, None, None, None, None],
@@ -23,7 +15,10 @@ class TestBoard(TestCase):
                [None, None, None, None, None, None, None, None, None, None],
                [None, None, None, None, None, None, None, None, None, None],
                ]
-       self.board_with_ships = [
+       return empty_board  
+   
+    def generate_board_with_ships(self):
+        board_with_ships = [
                 [self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], None, None, None, None, self.ships.all_ships[3]], 
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
@@ -35,7 +30,10 @@ class TestBoard(TestCase):
                 [None, None, None, None, None, None, None, None, None, None],
                 [None, None, None, None, None, None, None, None, None, None],
                 ]
-       self.board_with_a_hit = [
+        return board_with_ships 
+
+    def generate_board_with_hit(self):
+        board_with_a_hit = [
                 [self.ships.all_ships[0], 'Hit', self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], None, None, None, None, self.ships.all_ships[3]], 
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
@@ -47,8 +45,10 @@ class TestBoard(TestCase):
                 [None, None, None, None, None, None, None, None, None, None],
                 [None, None, None, None, None, None, None, None, None, None],
                 ]
-
-       self.board_with_a_miss = [
+        return board_with_a_hit
+    
+    def generate_board_with_a_miss(self):
+        board_with_a_miss = [
                 [self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], None, None, None, None, self.ships.all_ships[3]], 
                 ['Miss', None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
@@ -60,8 +60,10 @@ class TestBoard(TestCase):
                 [None, None, None, None, None, None, None, None, None, None],
                 [None, None, None, None, None, None, None, None, None, None],
                 ]
-
-       self.board_with_a_sunken_ship = [
+        return board_with_a_miss
+    
+    def generate_board_with_a_sunken_ship(self):
+        board_with_a_sunken_ship = [
                 ['Sunk', 'Sunk', 'Sunk', 'Sunk', 'Sunk', None, None, None, None, self.ships.all_ships[3]], 
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
@@ -73,92 +75,4 @@ class TestBoard(TestCase):
                 [None, None, None, None, None, None, None, None, None, None],
                 [None, None, None, None, None, None, None, None, None, None],
                 ]
-       
-    def test_Hit_is_added_to_board_if_the_user_hit_a_ship(self):
-        user_shot_choice = 'B1'
-        self.board.state = self.board_with_ships 
-        shot_result = 'Hit'
-        self.maxDiff = None 
-        self.board.update(user_shot_choice, shot_result)
-
-        self.assertEqual(self.board.state, self.board_with_a_hit)
-
-    def test_Miss_is_added_to_board_if_the_user_Misses_a_ship(self):
-        user_shot_choice = 'A2'
-        self.board.state = self.board_with_ships 
-        shot_result = 'Miss'
-        self.maxDiff = None 
-        self.board.update(user_shot_choice, shot_result)
-
-        self.assertEqual(self.board.state, self.board_with_a_miss)
-    
-    def test_when_ship_sinks_all_positions_where_ship_was_are_updated_to_str_Sunk(self):
-        user_shot_choice = 'A1'
-        self.board.state = self.board_with_ships 
-        self.board.state[0][0] = {
-                'name': 'Aircraft Carrier',
-                'size': 5,
-                'sunk': True,
-                'hit_locations': [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]],
-                }
-        shot_result = 'Sunk'
-        self.maxDiff = None 
-        self.board.update(user_shot_choice, shot_result)
-
-        self.assertEqual(self.board.state, self.board_with_a_sunken_ship)
-             
-    @patch('core.placement.Place.create_random_num', return_value = 0)
-    @patch('core.placement.Place.create_random_num', return_value = 0)
-    def test_ship_added_to_row_if_there_is_room(self, mock1, mock2):
-        place = Place()
-        all_ships = [{ 
-                'name': 'Aircraft Carrier',
-                'size': 5,
-                'health': 5,
-                'sunk': False,
-                }]
-        state_after_move = [
-                [all_ships[0], all_ships[0], all_ships[0], all_ships[0], all_ships[0], None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
-        ship_orientation = 'row'
-        self.board.add_to_board(all_ships, place, ship_orientation)
-
-        self.assertEqual(self.board.state, state_after_move)
-
-    @patch('core.placement.Place.create_random_num', return_value = 0)
-    @patch('core.placement.Place.create_random_num', return_value = 0)
-    def test_ship__added_to_column_if_there_is_room(self, mock1, mock2):
-        place = Place()
-        all_ships = [{ 
-                'name': 'Aircraft Carrier',
-                'size': 5,
-                'health': 5,
-                'sunk': False,
-                }]
-        state_after_move = [
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
-        ship_orientation = 'column'
-        self.board.add_to_board(all_ships, place, ship_orientation)
-
-        self.assertEqual(self.board.state, state_after_move)
-    
-    
+        return board_with_a_sunken_ship
--- tests/test_board_helper.py
+++ tests/test_board_helper.py
@@ -1,17 +1,15 @@
 from unittest import TestCase, mock
 from unittest.mock import patch, MagicMock
-from core.board import Board
-from core.placement import Place
-from core.validate import Validate
 from core.ships import Ships
 from helpers.board_helper import BoardHelper
 
-class TestBoard(TestCase):
-
+class TestBoardHelper(TestCase):
     def setUp(self):
-       self.board = Board()
-       self.ships = Ships()
-       self.empty_board = [
+        self.ships = Ships()
+        self.board_helper = BoardHelper(self.ships)    
+
+    def test_generate_empty_board_returns_an_empty_board(self):
+        empty_board = [
                [None, None, None, None, None, None, None, None, None, None],
                [None, None, None, None, None, None, None, None, None, None],
                [None, None, None, None, None, None, None, None, None, None],
@@ -23,7 +21,11 @@ class TestBoard(TestCase):
                [None, None, None, None, None, None, None, None, None, None],
                [None, None, None, None, None, None, None, None, None, None],
                ]
-       self.board_with_ships = [
+
+        self.assertEqual(self.board_helper.generate_empty_board(), empty_board)
+    
+    def test_generate_board_with_ships_returns_a_board_with_ships(self):
+        board_with_ships = [
                 [self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], None, None, None, None, self.ships.all_ships[3]], 
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
@@ -35,7 +37,11 @@ class TestBoard(TestCase):
                 [None, None, None, None, None, None, None, None, None, None],
                 [None, None, None, None, None, None, None, None, None, None],
                 ]
-       self.board_with_a_hit = [
+
+        self.assertEqual(self.board_helper.generate_board_with_ships(), board_with_ships)
+
+    def test_generate_board_with_hit_returns_a_board_with_a_hit(self):
+        board_with_a_hit = [
                 [self.ships.all_ships[0], 'Hit', self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], None, None, None, None, self.ships.all_ships[3]], 
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
@@ -48,7 +54,10 @@ class TestBoard(TestCase):
                 [None, None, None, None, None, None, None, None, None, None],
                 ]
 
-       self.board_with_a_miss = [
+        self.assertEqual(self.board_helper.generate_board_with_hit(), board_with_a_hit)
+
+    def test_generate_board_with_a_miss_returns_a_board_with_a_miss(self):
+        board_with_a_miss = [
                 [self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], self.ships.all_ships[0], None, None, None, None, self.ships.all_ships[3]], 
                 ['Miss', None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
@@ -61,7 +70,10 @@ class TestBoard(TestCase):
                 [None, None, None, None, None, None, None, None, None, None],
                 ]
 
-       self.board_with_a_sunken_ship = [
+        self.assertEqual(self.board_helper.generate_board_with_a_miss(), board_with_a_miss)
+
+    def test_generate_board_with_sunken_ship_returns_a_board_with_a_sunken_ship(self):
+        board_with_a_sunken_ship = [
                 ['Sunk', 'Sunk', 'Sunk', 'Sunk', 'Sunk', None, None, None, None, self.ships.all_ships[3]], 
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
                 [None, None, None, None, None, None, None, None, None, self.ships.all_ships[3]],
@@ -73,92 +85,7 @@ class TestBoard(TestCase):
                 [None, None, None, None, None, None, None, None, None, None],
                 [None, None, None, None, None, None, None, None, None, None],
                 ]
-       
-    def test_Hit_is_added_to_board_if_the_user_hit_a_ship(self):
-        user_shot_choice = 'B1'
-        self.board.state = self.board_with_ships 
-        shot_result = 'Hit'
-        self.maxDiff = None 
-        self.board.update(user_shot_choice, shot_result)
 
-        self.assertEqual(self.board.state, self.board_with_a_hit)
-
-    def test_Miss_is_added_to_board_if_the_user_Misses_a_ship(self):
-        user_shot_choice = 'A2'
-        self.board.state = self.board_with_ships 
-        shot_result = 'Miss'
-        self.maxDiff = None 
-        self.board.update(user_shot_choice, shot_result)
-
-        self.assertEqual(self.board.state, self.board_with_a_miss)
-    
-    def test_when_ship_sinks_all_positions_where_ship_was_are_updated_to_str_Sunk(self):
-        user_shot_choice = 'A1'
-        self.board.state = self.board_with_ships 
-        self.board.state[0][0] = {
-                'name': 'Aircraft Carrier',
-                'size': 5,
-                'sunk': True,
-                'hit_locations': [[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]],
-                }
-        shot_result = 'Sunk'
-        self.maxDiff = None 
-        self.board.update(user_shot_choice, shot_result)
-
-        self.assertEqual(self.board.state, self.board_with_a_sunken_ship)
-             
-    @patch('core.placement.Place.create_random_num', return_value = 0)
-    @patch('core.placement.Place.create_random_num', return_value = 0)
-    def test_ship_added_to_row_if_there_is_room(self, mock1, mock2):
-        place = Place()
-        all_ships = [{ 
-                'name': 'Aircraft Carrier',
-                'size': 5,
-                'health': 5,
-                'sunk': False,
-                }]
-        state_after_move = [
-                [all_ships[0], all_ships[0], all_ships[0], all_ships[0], all_ships[0], None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
-        ship_orientation = 'row'
-        self.board.add_to_board(all_ships, place, ship_orientation)
-
-        self.assertEqual(self.board.state, state_after_move)
-
-    @patch('core.placement.Place.create_random_num', return_value = 0)
-    @patch('core.placement.Place.create_random_num', return_value = 0)
-    def test_ship__added_to_column_if_there_is_room(self, mock1, mock2):
-        place = Place()
-        all_ships = [{ 
-                'name': 'Aircraft Carrier',
-                'size': 5,
-                'health': 5,
-                'sunk': False,
-                }]
-        state_after_move = [
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [all_ships[0], None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
-        ship_orientation = 'column'
-        self.board.add_to_board(all_ships, place, ship_orientation)
-
-        self.assertEqual(self.board.state, state_after_move)
-    
-    
+        self.assertEqual(self.board_helper.generate_board_with_a_sunken_ship(), board_with_a_sunken_ship)
+        
+        
