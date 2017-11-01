8e5fe01502380a455b61eea1610f7cf3fbdd430b
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-11-01 14:41:58 -0400

    add comp_board and human_board

--- play.py
+++ play.py
@@ -1,33 +1,35 @@
 from core import board, ui, validate, ships, placement
 
 class Game:
-    def __init__(self, board, ui, validate, place):
+    def __init__(self, comp_board, human_board, ui, validate, place):
         self.ui = ui
-        self.board = board
+        self.comp_board = comp_board 
         self.validate = validate
         self.place = place
+        self.human_board = human_board
 
     def play(self):
         self.ui.display("Welcome to Battleship")
         board_full = False
         ship_orientation = 'row'
-        self.board.add_to_board(self.place, ship_orientation)
+        self.comp_board.add_to_board(self.place, ship_orientation)
 
         while not board_full:
             self.ui.display("Take your best shot")
-            self.ui.display(self.ui.format(self.board.state, self.board.ships.all_ships))
-            spot_choice = self.validate.spot_occupied(self.board.state, self.ui, self.board.ships.all_ships)
-            shot_result = self.validate.hit_ship(self.board.state, spot_choice, self.board.ships.all_ships, self.ui)
-            self.board.update(spot_choice, shot_result)
-            board_full = self.validate.board_full(self.board.state) 
+            self.ui.display(self.ui.format(self.comp_board.state, self.comp_board.ships.all_ships))
+            spot_choice = self.validate.spot_occupied(self.comp_board.state, self.ui, self.comp_board.ships.all_ships)
+            shot_result = self.validate.hit_ship(self.comp_board.state, spot_choice, self.comp_board.ships.all_ships, self.ui)
+            self.comp_board.update(spot_choice, shot_result)
+            board_full = self.validate.board_full(self.comp_board.state) 
 
 
 if __name__ == "__main__":
     ships = ships.Ships()
-    board = board.Board(ships)
+    comp_board = board.Board(ships)
+    human_board = board.Board(ships)
     ui = ui.TerminalUi()
     validate = validate.Validate()
     place = placement.Place()
-    game = Game(board, ui, validate, place)
+    game = Game(comp_board, human_board, ui, validate, place)
     game.play()
 
--- tests/test_game.py
+++ tests/test_game.py
@@ -13,7 +13,8 @@ class TestPlayGame(TestCase):
     def test_play_runs_the_correct_methods(self):
         ships = Ships()
         ui = TerminalUi()
-        board = Board(ships)
+        human_board = Board(ships)
+        comp_board = Board(ships)
         validate = Validate()
         place = Place()
 
@@ -26,9 +27,9 @@ class TestPlayGame(TestCase):
         ship_size = 5
         hit = False
 
-        board.state = MagicMock(return_value = board_state)
-        board.add_to_board = MagicMock(return_value = board_state_after_ship_placed)
-        board.update = MagicMock()
+        comp_board.state = MagicMock(return_value = board_state)
+        comp_board.add_to_board = MagicMock(return_value = board_state_after_ship_placed)
+        comp_board.update = MagicMock()
         validate.spot_occupied = MagicMock(return_value = user_shot_choice)
         validate.board_full = MagicMock(return_value = True)
         validate.hit_ship = MagicMock(return_value = False)
@@ -36,12 +37,12 @@ class TestPlayGame(TestCase):
         ui.get_input = MagicMock(return_value = user_shot_choice)
         ui.display = MagicMock()
 
-        new_game = Game(board, ui, validate, place)
+        new_game = Game(comp_board, human_board, ui, validate, place)
         new_game.play()
 
-        board.add_to_board.assert_called_with(place, ship_orientation)
+        comp_board.add_to_board.assert_called_with(place, ship_orientation)
         ui.display.assert_called()
-        validate.board_full.assert_called_with(board.state)
-        validate.spot_occupied.assert_called_with(board.state, ui, board.ships.all_ships)
-        validate.hit_ship(board.state, user_shot_choice, board.ships.all_ships) 
-        board.update.assert_called_with(user_shot_choice, hit)
+        validate.board_full.assert_called_with(comp_board.state)
+        validate.spot_occupied.assert_called_with(comp_board.state, ui, comp_board.ships.all_ships)
+        validate.hit_ship(comp_board.state, user_shot_choice, comp_board.ships.all_ships) 
+        comp_board.update.assert_called_with(user_shot_choice, hit)
