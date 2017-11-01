3399764cf82e90680ef33724e5c28bbfcf8de886
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-11-01 11:44:56 -0400

    stop ships from wrapping on board edge

--- core/placement.py
+++ core/placement.py
@@ -10,10 +10,10 @@ class Place:
         while ship_fits != True:
             row_int = self.create_random_num()
             col_int = self.create_random_num()
-            if (col_int + ship_size) > len(board_state): ship_fits = False 
             requested_row = board_state[row_int]
             slice_of_row = requested_row[row_int : row_int + (len(requested_row) - ship_size)]
             ship_fits = all([spot == None for spot in slice_of_row])
+            if (col_int + ship_size) > len(board_state): ship_fits = False 
         return row_int, col_int 
 
     def find_space_in_column(self, board_state, ship_size, result=''):
@@ -21,9 +21,9 @@ class Place:
         while ship_fits != True:
             row_int = self.create_random_num()
             col_int = self.create_random_num() 
-            if (row_int + ship_size) > len(board_state): ship_fits = False
             requested_col = [sub_list[col_int] for sub_list in board_state]
             slice_of_col = requested_col[row_int : row_int + (len(requested_col) - ship_size)]
             ship_fits = all([spot == None for spot in slice_of_col])
+            if (row_int + ship_size) > len(board_state): ship_fits = False
         return row_int, col_int
     
