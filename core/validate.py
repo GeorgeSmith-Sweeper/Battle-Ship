6b1eb93f5a1afcd5366efb2cf9bb81e393400ac7
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-11-13 14:18:24 -0500

    linted validate.py

--- core/validate.py
+++ core/validate.py
@@ -1,13 +1,14 @@
 import helpers.constants as constants
 
+
 class Validate:
 
     def __init__(self):
-        self.col_lets = constants.COL_LETS 
-        self.row_nums = constants.ROW_NUMS 
-        self.all_spots = constants.ALL_SPOTS 
-        self.rows = constants.ROWS 
-        self.columns = constants.COLUMNS 
+        self.col_lets = constants.COL_LETS
+        self.row_nums = constants.ROW_NUMS
+        self.all_spots = constants.ALL_SPOTS
+        self.rows = constants.ROWS
+        self.columns = constants.COLUMNS
 
     def split_user_shot(self, shot_choice):
         user_let = shot_choice[0]
@@ -27,17 +28,17 @@ class Validate:
             ui.display('Spot does not exist, Try again')
             user_shot_choice = ui.get_input('>>')
         return user_shot_choice
- 
+
     def spot_occupied(self, board, ui):
         user_shot_choice = self.spot_exists(ui)
         current_spot = self.get_current_spot(board.state, user_shot_choice)
 
-        while current_spot is not None and current_spot not in board.all_ships: 
+        while current_spot is not None and current_spot not in board.all_ships:
             ui.display('That spot is occupied. Pick a different spot')
             user_shot_choice = self.spot_exists(ui)
             current_spot = self.get_current_spot(board.state, user_shot_choice)
         return user_shot_choice
-   
+
     def hit_ship(self, board, spot_choice, ui):
         current_spot = self.get_current_spot(board.state, spot_choice)
         for ship in range(len(board.all_ships)):
@@ -46,7 +47,7 @@ class Validate:
                 if self.is_ship_sunk(board.all_ships[ship], ui):
                     return 'Sunk'
                 ui.display('You hit the ' + board.all_ships[ship]['name'] + '!')
-                return 'Hit'      
+                return 'Hit'
         ui.display('Miss!')
         return 'Miss'
 
@@ -55,20 +56,19 @@ class Validate:
         row = self.rows[user_num]
         column = self.columns[user_let]
 
-        current_ship['hit_locations'].append([row, column])  
+        current_ship['hit_locations'].append([row, column])
         return current_ship
 
     def is_ship_sunk(self, current_ship, ui):
-        if len(current_ship['hit_locations']) == current_ship['size']: 
+        if len(current_ship['hit_locations']) == current_ship['size']:
             ui.display("You sunk the " + current_ship['name'] + '!')
             return True
-        return False 
+        return False
 
     def all_ships_sunk(self, board):
         all_sunk = True
         for row in range(0, len(board.state)):
             for ele in board.state[row]:
                 if ele in board.all_ships:
-                   return False
-        return all_sunk 
- 
+                    return False
+        return all_sunk
