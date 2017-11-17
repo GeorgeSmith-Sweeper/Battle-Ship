27db8b94d0010941dd2eef7f38ad9084d8e49baf
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-11-17 15:58:35 -0500

    create location for ship

--- core/placement.py
+++ core/placement.py
@@ -12,22 +12,22 @@ class Place:
         col_int = self.create_random_num(board_state)
         return row_int, col_int
 
-    def find_space_in_row(self, board_state, ship_size, result=''):
-        ship_fits = result
+    def find_space_in_row(self, board_state, ship_size):
+        ship_fits = ''
         while ship_fits is not True:
             row_int, col_int = self.get_random_row_and_column(board_state)
             requested_row = board_state[row_int]
-            slice_of_row = requested_row[row_int: row_int + (len(requested_row) - ship_size)]
+            slice_of_row = self._location_for_ship(requested_row, row_int, ship_size)
             ship_fits = self._is_ship_too_long(col_int, ship_size, board_state)
             ship_fits = self._are_spaces_open(slice_of_row)
         return row_int, col_int
 
-    def find_space_in_column(self, board_state, ship_size, result=''):
-        ship_fits = result
+    def find_space_in_column(self, board_state, ship_size):
+        ship_fits = ''
         while ship_fits is not True:
             row_int, col_int = self.get_random_row_and_column(board_state)
             requested_col = [sub_list[col_int] for sub_list in board_state]
-            slice_of_col = requested_col[row_int: row_int + (len(requested_col) - ship_size)]
+            slice_of_col = self._location_for_ship(requested_col, row_int, ship_size)
             ship_fits = self._is_ship_too_long(row_int, ship_size, board_state)
             ship_fits = self._are_spaces_open(slice_of_col)
         return row_int, col_int
@@ -39,3 +39,6 @@ class Place:
         if (start + ship_size) > len(board_state):
             return False
         return True
+
+    def _location_for_ship(self, requested_location, row_int, ship_size):
+        return requested_location[row_int: row_int + (len(requested_location) - ship_size)]
