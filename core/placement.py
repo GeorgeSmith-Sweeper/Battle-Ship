0615d0ea5a473e220e4e36b22a8d4297358f70c1
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-11-20 17:02:58 -0500

    revert back to version that works

--- core/placement.py
+++ core/placement.py
@@ -21,23 +21,15 @@ class Place:
     def _location_for_ship(self, requested_location, row_int, ship_size):
         return requested_location[row_int: row_int + (len(requested_location) - ship_size)]
 
-    def _will_ship_fit(self, board_slice, ship_start, ship_size, board_state):
-        are_open = self._are_spaces_open(board_slice)
-        too_long = self._is_ship_too_long(ship_start, ship_size, board_state)
-        return too_long is False and are_open is True
-
     def find_space_in_row(self, board_state, ship_size, result=''):
         ship_fits = result
         while ship_fits is not True:
             row_int, col_int = self.get_random_row_and_column(board_state)
             requested_row = board_state[row_int]
             slice_of_row = self._location_for_ship(requested_row, row_int, ship_size)
-            ship_fits = all([spot is None for spot in slice_of_row])
+            ship_fits = self._are_spaces_open(slice_of_row)
             if (col_int + ship_size) > len(board_state):
                 ship_fits = False
-            '''
-            ship_fits = self._will_ship_fit(slice_of_row, row_int, ship_size, board_state)
-            '''
         return row_int, col_int
 
     def find_space_in_column(self, board_state, ship_size, result=''):
@@ -46,10 +38,7 @@ class Place:
             row_int, col_int = self.get_random_row_and_column(board_state)
             requested_col = [sub_list[col_int] for sub_list in board_state]
             slice_of_col = self._location_for_ship(requested_col, row_int, ship_size)
-            ship_fits = ship_fits = all([spot is None for spot in slice_of_col])
+            ship_fits = self._are_spaces_open(slice_of_col)
             if (row_int + ship_size) > len(board_state):
                 ship_fits = False
-            '''
-            ship_fits = self._will_ship_fit(slice_of_col, row_int, ship_size, board_state)
-            '''
         return row_int, col_int
