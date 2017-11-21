ac77b2c73bfca5d6aa8ca79a2bb80a49bfca5e02
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-11-21 16:35:52 -0500

    create _get_column, get_row

--- core/placement.py
+++ core/placement.py
@@ -19,40 +19,32 @@ class Place:
         if (ship_start + ship_size) > len(board_state):
             return False
         return open_spaces
-    '''
-    def _open_and_proper_size(self, loc_slice, start, ship_size, board_state):
-        spaces_open = self._are_spaces_open(loc_slice)
-        too_long = self._is_ship_too_long(start, ship_size, board_state)
-        if too_long is True:
-            return False
-        if spaces_open is True:
-            return True
-    '''
+
     def _location_for_ship(self, requested_location, row_int, ship_size):
         return requested_location[row_int: row_int + (len(requested_location) - ship_size)]
+
+    def _get_row(self, row_int, board_state):
+        return board_state[row_int]
+
+    def _get_column(self, col_int, board_state):
+        return [sub_list[col_int] for sub_list in board_state]
 
     def find_space_in_row(self, board_state, ship_size, result=''):
         ship_fits = result
         while ship_fits is not True:
             row_int, col_int = self.get_random_row_and_column(board_state)
-            requested_row = board_state[row_int]
+            requested_row = self._get_row(row_int, board_state)
             slice_of_row = self._location_for_ship(requested_row, row_int, ship_size)
-            ship_fits = self._are_spaces_open(slice_of_row)
-            ship_fits = self._ship_over_edge(col_int, ship_size, board_state, ship_fits)
-            '''
-            ship_fits = self._open_and_proper_size(slice_of_row, col_int, ship_size, board_state)
-            '''
+            open_spaces = self._are_spaces_open(slice_of_row)
+            ship_fits = self._ship_over_edge(col_int, ship_size, board_state, open_spaces)
         return row_int, col_int
 
     def find_space_in_column(self, board_state, ship_size, result=''):
         ship_fits = result
         while ship_fits is not True:
             row_int, col_int = self.get_random_row_and_column(board_state)
-            requested_col = [sub_list[col_int] for sub_list in board_state]
+            requested_col = self._get_column(col_int, board_state)
             slice_of_col = self._location_for_ship(requested_col, row_int, ship_size)
-            ship_fits = self._are_spaces_open(slice_of_col)
-            ship_fits = self._ship_over_edge(row_int, ship_size, board_state, ship_fits)
-            '''
-            ship_fits = self._open_and_proper_size(slice_of_col, row_int, ship_size, board_state)
-            '''
+            open_spaces = self._are_spaces_open(slice_of_col)
+            ship_fits = self._ship_over_edge(row_int, ship_size, board_state, open_spaces)
         return row_int, col_int
