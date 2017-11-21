1992931455a50fc35b5c17b856d533685b99bc09
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-11-21 16:17:40 -0500

    rename _too_long to _ship_over_edge

--- core/placement.py
+++ core/placement.py
@@ -14,10 +14,12 @@ class Place:
 
     def _are_spaces_open(self, location):
         return all([spot is None for spot in location])
-    '''
-    def _is_ship_too_long(self, start, ship_size, board_state):
-        return (start + ship_size) > len(board_state)
 
+    def _ship_over_edge(self, ship_start, ship_size, board_state, open_spaces):
+        if (ship_start + ship_size) > len(board_state):
+            return False
+        return open_spaces
+    '''
     def _open_and_proper_size(self, loc_slice, start, ship_size, board_state):
         spaces_open = self._are_spaces_open(loc_slice)
         too_long = self._is_ship_too_long(start, ship_size, board_state)
@@ -35,12 +37,11 @@ class Place:
             row_int, col_int = self.get_random_row_and_column(board_state)
             requested_row = board_state[row_int]
             slice_of_row = self._location_for_ship(requested_row, row_int, ship_size)
+            ship_fits = self._are_spaces_open(slice_of_row)
+            ship_fits = self._ship_over_edge(col_int, ship_size, board_state, ship_fits)
             '''
             ship_fits = self._open_and_proper_size(slice_of_row, col_int, ship_size, board_state)
             '''
-            ship_fits = self._are_spaces_open(slice_of_row)
-            if (col_int + ship_size) > len(board_state):
-                ship_fits = False
         return row_int, col_int
 
     def find_space_in_column(self, board_state, ship_size, result=''):
@@ -49,10 +50,9 @@ class Place:
             row_int, col_int = self.get_random_row_and_column(board_state)
             requested_col = [sub_list[col_int] for sub_list in board_state]
             slice_of_col = self._location_for_ship(requested_col, row_int, ship_size)
+            ship_fits = self._are_spaces_open(slice_of_col)
+            ship_fits = self._ship_over_edge(row_int, ship_size, board_state, ship_fits)
             '''
             ship_fits = self._open_and_proper_size(slice_of_col, row_int, ship_size, board_state)
             '''
-            ship_fits = self._are_spaces_open(slice_of_col)
-            if (row_int + ship_size) > len(board_state):
-                ship_fits = False
         return row_int, col_int
