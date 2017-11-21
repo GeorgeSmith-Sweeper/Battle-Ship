ed8e627672e9ced687d3f59d22df5935a1c26c2b
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-11-21 13:30:56 -0500

    remove comments, make methods private

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -24,12 +24,12 @@ class TestAi(TestCase):
         shot_result, current_ship = result
         board_with_ships = self.board_helper.generate_board_with_ships()
         self.human_board = MagicMock(state=board_with_ships, all_ships=self.human_board.all_ships)
-        self.ai.choose_random_spot = MagicMock(return_value=ai_shot)
+        self.ai._choose_random_spot = MagicMock(return_value=ai_shot)
         self.validate.shot_result = MagicMock(return_value=result)
 
         self.ai.shoots_at_board(self.human_board)
 
-        self.ai.choose_random_spot.assert_called()
+        self.ai._choose_random_spot.assert_called()
         self.human_board.update.assert_called_with(ai_shot, shot_result)
 
     def test_room_from_top_edge_returns_True_if_spot_is_not_on_the_edge(self):
@@ -84,37 +84,37 @@ class TestAi(TestCase):
         column = 'B'
         row = '2'
 
-        self.assertEqual(self.ai.get_spot_above(column, row), 'B1')
+        self.assertEqual(self.ai._get_spot_above(column, row), 'B1')
 
     def test_get_spot_below_returns_the_space_below_selected_spot(self):
         column = 'B'
         row = '2'
 
-        self.assertEqual(self.ai.get_spot_below(column, row, self.board.state), 'B3')
+        self.assertEqual(self.ai._get_spot_below(column, row, self.board.state), 'B3')
 
     def test_get_spot_below_returns_selected_spot_if_there_is_no_space_below(self):
         column = 'B'
         row = '10'
 
-        self.assertEqual(self.ai.get_spot_below(column, row, self.board.state), 'B10')
+        self.assertEqual(self.ai._get_spot_below(column, row, self.board.state), 'B10')
 
     def test_get_spot_to_left_returns_the_space_to_the_left_of_selected_spot(self):
         column = 'B'
         row = '2'
 
-        self.assertEqual(self.ai.get_spot_to_left(column, row), 'A2')
+        self.assertEqual(self.ai._get_spot_to_left(column, row), 'A2')
 
     def test_get_spot_to_right_returns_the_space_to_the_right_of_selected_spot(self):
         column = 'B'
         row = '2'
 
-        self.assertEqual(self.ai.get_spot_to_right(column, row, self.board.state), 'C2')
+        self.assertEqual(self.ai._get_spot_to_right(column, row, self.board.state), 'C2')
 
     def test_get_spot_to_right_returns_selected_spot_if_there_is_no_spot_to_right(self):
         column = 'J'
         row = '10'
 
-        self.assertEqual(self.ai.get_spot_below(column, row, self.board.state), 'J10')
+        self.assertEqual(self.ai._get_spot_below(column, row, self.board.state), 'J10')
 
     def test_remove_none_from_list_returns_a_list_without_nones(self):
         list_with_nones = ['B1', 'B2', None, None]
@@ -126,13 +126,13 @@ class TestAi(TestCase):
     def test_get_surrounding_spots_adds_the_spots_to_the_next_shot_list(self):
         selected_spot = 'B2'
         correct_spots = ['B1', 'B3', 'A2', 'C2']
-        self.ai.get_surrounding_spots(selected_spot, self.human_board.state)
+        self.ai._get_surrounding_spots(selected_spot, self.human_board.state)
 
         self.assertEqual(self.ai.next_shots_list, correct_spots)
 
     def test_choose_random_spot_picks_a_spot_chooses_spaces_from_available_spaces(self):
         all_spots_list = self.board_helper.generate_all_spots()
-        random_spot = self.ai.choose_random_spot()
+        random_spot = self.ai._choose_random_spot()
 
         self.assertIn(random_spot, all_spots_list)
 
@@ -141,33 +141,15 @@ class TestAi(TestCase):
         spot = 'B1'
         board_with_a_hit = self.board_helper.generate_board_with_hit()
         self.board.state = self.board_helper.generate_board_with_ships()
-        self.ai.unified_shot(self.board, spot)
+        self.ai._unified_shot(self.board, spot)
 
         self.assertEqual(self.board.state, board_with_a_hit)
 
-    @patch('core.ai.Ai.choose_random_spot', return_value='B1')
+    @patch('core.ai.Ai._choose_random_spot', return_value='B1')
     def test_unified_shot_updates_the_board_with_a_random_shot(self, mock):
         board_with_a_hit = self.board_helper.generate_board_with_hit()
         self.board.state = self.board_helper.generate_board_with_ships()
         random_spot = 'B1'
-        self.ai.unified_shot(self.board, random_spot)
+        self.ai._unified_shot(self.board, random_spot)
 
         self.assertEqual(self.board.state, board_with_a_hit)
-
-    '''
-    def test_intelligent_shot_updates_the_board_with_shots_from_nxt_shot_list(self):
-        self.ai.next_shots_list = ['B1']
-        board_with_a_hit = self.board_helper.generate_board_with_hit()
-        self.board.state = self.board_helper.generate_board_with_ships()
-        self.ai.intelligent_shot(self.board)
-
-        self.assertEqual(self.board.state, board_with_a_hit)
-
-    @patch('core.ai.Ai.choose_random_spot', return_value='B1')
-    def test_random_shot_updates_the_board_with_a_random_shot(self, mock):
-        board_with_a_hit = self.board_helper.generate_board_with_hit()
-        self.board.state = self.board_helper.generate_board_with_ships()
-        self.ai.random_shot(self.board)
-
-        self.assertEqual(self.board.state, board_with_a_hit)
-    '''
