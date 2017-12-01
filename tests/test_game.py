d07970178c981ba66ec8b44d7557188598e003e2
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-12-01 13:36:20 -0500

    make use of setUp, clean up play test

--- tests/test_game.py
+++ tests/test_game.py
@@ -21,14 +21,6 @@ class TestPlayGame(TestCase):
         self.place = Place()
 
     def test_play_runs_the_correct_methods(self):
-        ui = TerminalUi()
-        human_board = Board()
-        comp_board = Board()
-        board_helper = BoardHelper(comp_board.all_ships)
-        validate = Validate()
-        place = Place()
-        ai = Ai(validate)
-
         board_state = [[None]]
         board_state_after_ship_placed = [['AC', 'AC', 'AC', 'AC', 'AC']]
         user_shot_choice = 'A1'
@@ -36,37 +28,36 @@ class TestPlayGame(TestCase):
         shot_result = 'Hit'
         current_ship = {'name': 'Aircraft Carrier', 'size': 5, 'hit_locations': [[0, 0]]}
 
-        comp_board = MagicMock()
-        comp_board.state = MagicMock(return_value=board_state)
-        comp_board.add_to_board = MagicMock(return_value=board_state_after_ship_placed)
-        comp_board.update = MagicMock()
-        comp_board.all_ships = MagicMock(return_value=board_helper.all_ships)
-        validate = MagicMock()
-        validate.shot_result = MagicMock(return_value=('Hit', current_ship))
-        validate.all_ships_sunk = MagicMock(side_effect=[False, True])
-        ai.shoot_at_board = MagicMock(return_value=('Hit', current_ship))
-        ui.get_input = MagicMock(return_value=user_shot_choice)
-        ui.display = MagicMock()
-        ui.terminal_board = MagicMock()
-        ui.ship_messages = MagicMock()
+        self.comp_board = MagicMock()
+        self.comp_board.state = MagicMock(return_value=board_state)
+        self.comp_board.add_to_board = MagicMock(return_value=board_state_after_ship_placed)
+        self.comp_board.update = MagicMock()
+        self.comp_board.all_ships = MagicMock(return_value=self.board_helper.all_ships)
+        self.validate = MagicMock()
+        self.validate.shot_result = MagicMock(return_value=('Hit', current_ship))
+        self.validate.all_ships_sunk = MagicMock(side_effect=[False, True])
+        self.ai.shoot_at_board = MagicMock(return_value=('Hit', current_ship))
+        self.ui.get_input = MagicMock(return_value=user_shot_choice)
+        self.ui.display = MagicMock()
+        self.ui.terminal_board = MagicMock()
+        self.ui.ship_messages = MagicMock()
 
-        new_game = Game(comp_board, human_board, ai, ui, validate, place)
+        new_game = Game(self.comp_board, self.human_board, self.ai, self.ui, self.validate, self.place)
         new_game.play()
 
-        comp_board.add_to_board.assert_called_with(place, ship_orientation)
-        ui.display.assert_called()
-        comp_board.update.assert_called_with(user_shot_choice, shot_result)
-        validate.all_ships_sunk.assert_called()
-        ai.shoot_at_board.assert_called_with(human_board)
-        ui.terminal_board.assert_called()
-        ui.ship_messages.assert_called()
+        self.comp_board.add_to_board.assert_called_with(self.place, ship_orientation)
+        self.ui.display.assert_called()
+        self.comp_board.update.assert_called_with(user_shot_choice, shot_result)
+        self.validate.all_ships_sunk.assert_called()
+        self.ai.shoot_at_board.assert_called_with(self.human_board)
+        self.ui.terminal_board.assert_called()
+        self.ui.ship_messages.assert_called()
 
     @patch('core.validate.Validate.spot_is_legal', return_value=True)
     @patch('core.ui.TerminalUi.get_input', return_value='A1')
     def test_human_player_returns_the_shot_result_and_ship(self, mock1, mock2):
         new_game = Game(self.comp_board, self.human_board, self.ai, self.ui, self.validate, self.place)
-        board_helper = BoardHelper(self.comp_board.all_ships)
-        board_with_ships = board_helper.generate_board_with_ships()
+        board_with_ships = self.board_helper.generate_board_with_ships()
         board = MagicMock(state=board_with_ships, all_ships=self.comp_board.all_ships)
         expected_ship = {
             'name': 'Aircraft Carrier',
