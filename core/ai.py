f5fd6bc046fc188ad4142c0e0216a3e0dd7b1b00
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-12-11 14:49:39 -0500

    rename variables to what they represent

--- core/ai.py
+++ core/ai.py
@@ -27,38 +27,38 @@ class Ai:
 
     def _computer_shot(self, board, spot):
         shot_result, current_ship = self.validate.shot_result(board, spot)
-        self._plan_next_moves(shot_result, spot, board)
+        self._plan_next_moves(shot_result, spot)
         board.update(spot, shot_result)
         self._remove_duplicated_spots(spot, self.next_shots_list)
         self._remove_spot_from_choices(spot)
         return shot_result, current_ship
 
-    def _plan_next_moves(self, shot_result, shot_location, board):
+    def _plan_next_moves(self, shot_result, shot_location):
         if shot_result == HIT:
-            self._get_surrounding_spots(shot_location, board)
+            self._get_surrounding_spots(shot_location)
 
-    def _get_surrounding_spots(self, selected_spot, board):
-        letter, num = self.validate.split_user_shot(selected_spot)
+    def _get_surrounding_spots(self, selected_spot):
+        column, row = self.validate.split_user_shot(selected_spot)
 
-        spot_above = self._get_spot(num, letter, num, -1, 0, -1, board, self.row_nums)
-        spot_left = self._get_spot(num, letter, letter, 0, -1, -1, board, self.col_letters)
-        spot_below = self._get_spot(num, letter, num, 1, 0, 1, board, self.row_nums)
-        spot_right = self._get_spot(num, letter, letter, 0, 1, 1, board, self.col_letters)
+        spot_above = self._get_spot(row, column, row, -1, 0, -1, self.row_nums)
+        spot_left = self._get_spot(row, column, column, 0, -1, -1, self.col_letters)
+        spot_below = self._get_spot(row, column, row, 1, 0, 1, self.row_nums)
+        spot_right = self._get_spot(row, column, column, 0, 1, 1, self.col_letters)
 
         gathered_spots = list((spot_above, spot_below, spot_left, spot_right))
         self.next_shots_list.extend(self._remove_none_from_list(gathered_spots))
 
-    def _get_spot(self, row, column, direction, offset, column_offset, check_offset, board, marker_list):
-        if self._room_around_space(direction, check_offset, board, marker_list):
+    def _get_spot(self, row, column, start_spot, offset, column_offset, check_offset, marker_list):
+        if self._room_around_space(start_spot, check_offset, marker_list):
             spot = self._get_coordinates(row, column, offset, column_offset)
             spot = self._legal_space(spot)
         else:
             spot = None
         return spot
 
-    def _room_around_space(self, spot, offset, board, marker_list):
+    def _room_around_space(self, spot, offset, marker_list):
         index = self._find_index(marker_list, spot, offset)
-        return index >= 0 and index < len(board.state)
+        return index >= 0 and index < len(marker_list)
 
     def _get_coordinates(self, row, column, offset, column_offset):
         letter = self.col_letters[self._find_index(self.col_letters, column, column_offset)]
