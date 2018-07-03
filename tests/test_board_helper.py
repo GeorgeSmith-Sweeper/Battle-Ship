451a48cd5a414490b72eb4db4a7c936503ff481e
George Smith-Sweeper <george.smsweeper@gmail.com>
2018-07-03 13:03:18 -0400

    fix indentation issues for collections

--- tests/test_board_helper.py
+++ tests/test_board_helper.py
@@ -28,115 +28,115 @@ class TestBoardHelper(TestCase):
         ]
 
         self.assertEqual(self.board_helper.generate_empty_board(), empty_board)
-    
+
     def test_generate_all_occupied_but_one_returns_a_board_with_one_open_spot(self):
         all_occupied_but_one = [
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               [None, 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-               ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
-              ]
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            [None, 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+            ['Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss', 'Miss'],
+        ]
 
         self.assertEqual(self.board_helper.generate_all_but_one(), all_occupied_but_one)
-    
+
     def test_generate_full_board_returns_a_board_with_all_spots_filled(self):
         full_board = [
-                ['Sunk', "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
-                ['Sunk', "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
-                ['Sunk', "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
-                ['Sunk', "Miss", "Miss", "Miss", 'Sunk', "Miss", "Miss", "Miss", "Miss", "Miss"],
-                ['Sunk', 'Sunk', "Miss", "Miss", 'Sunk', "Miss", "Miss", "Miss", "Miss", "Miss"],
-                ["Miss", 'Sunk', "Miss", "Miss", 'Sunk', "Miss", "Miss", "Miss", "Miss", "Miss"],
-                ["Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
-                ["Miss", "Miss", "Miss", "Miss", "Miss", "Miss", 'Sunk', 'Sunk', 'Sunk', 'Sunk'],
-                ["Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
-                ['Sunk', 'Sunk', 'Sunk', "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
-              ]
-        
+            ['Sunk', "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
+            ['Sunk', "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
+            ['Sunk', "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
+            ['Sunk', "Miss", "Miss", "Miss", 'Sunk', "Miss", "Miss", "Miss", "Miss", "Miss"],
+            ['Sunk', 'Sunk', "Miss", "Miss", 'Sunk', "Miss", "Miss", "Miss", "Miss", "Miss"],
+            ["Miss", 'Sunk', "Miss", "Miss", 'Sunk', "Miss", "Miss", "Miss", "Miss", "Miss"],
+            ["Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
+            ["Miss", "Miss", "Miss", "Miss", "Miss", "Miss", 'Sunk', 'Sunk', 'Sunk', 'Sunk'],
+            ["Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
+            ['Sunk', 'Sunk', 'Sunk', "Miss", "Miss", "Miss", "Miss", "Miss", "Miss", "Miss"],
+        ]
+
         self.assertEqual(self.board_helper.generate_full_board(), full_board)
 
     def test_generate_board_with_ships_returns_a_board_with_ships(self):
         board_with_ships = [
-                [self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], None, None, None, None, self.board.all_ships[3]], 
-                [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
+            [self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+        ]
 
         self.assertEqual(self.board_helper.generate_board_with_ships(), board_with_ships)
 
     def test_generate_board_with_hit_returns_a_board_with_a_hit(self):
         board_with_a_hit = [
-                [self.board.all_ships[0], 'Hit', self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], None, None, None, None, self.board.all_ships[3]], 
-                [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
+            [self.board.all_ships[0], 'Hit', self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+        ]
 
         self.assertEqual(self.board_helper.generate_board_with_hit(), board_with_a_hit)
 
     def test_generate_board_with_a_miss_returns_a_board_with_a_miss(self):
         board_with_a_miss = [
-                [self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], None, None, None, None, self.board.all_ships[3]], 
-                ['Miss', None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
+            [self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], None, None, None, None, self.board.all_ships[3]],
+            ['Miss', None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+        ]
 
         self.assertEqual(self.board_helper.generate_board_with_a_miss(), board_with_a_miss)
+
     def test_generate_board_with_hit_and_miss_returns_a_board_with_a_hit_and_miss(self):
         board_with_hit_and_miss = [
-                ['Hit', self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], None, None, None, None, self.board.all_ships[3]], 
-                ['Miss', None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
+            ['Hit', self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], self.board.all_ships[0], None, None, None, None, self.board.all_ships[3]],
+            ['Miss', None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+        ]
 
         self.assertEqual(self.board_helper.generate_board_with_hit_and_miss(), board_with_hit_and_miss)
 
     def test_generate_board_with_sunken_ship_returns_a_board_with_a_sunken_ship(self):
         board_with_a_sunken_ship = [
-                ['Sunk', 'Sunk', 'Sunk', 'Sunk', 'Sunk', None, None, None, None, self.board.all_ships[3]], 
-                [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
-                [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
-                [None, None, None, None, None, None, None, None, None, None],
-                [None, None, None, None, None, None, None, None, None, None],
-                ]
+            ['Sunk', 'Sunk', 'Sunk', 'Sunk', 'Sunk', None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, None, None, None, None, None, self.board.all_ships[3]],
+            [None, None, None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, self.board.all_ships[4], None, None, self.board.all_ships[2], None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1], self.board.all_ships[1]],
+            [None, None, None, None, None, None, None, None, None, None],
+            [None, None, None, None, None, None, None, None, None, None],
+        ]
 
         self.assertEqual(self.board_helper.generate_board_with_a_sunken_ship(), board_with_a_sunken_ship)
-        
